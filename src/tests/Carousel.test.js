160f05443fde7a6788e7ee6e731693a8a4825ee7
Jim <omesadev@outlook.com>
2021-10-27 06:20:17 -0400

    :sparkles: Add auto-advance support to carousel

--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -26,14 +26,18 @@ describe('Carousel', () => {
 
 
   describe('component with HOC', () => {
-    let wrapper;
+    let mounted;
 
     beforeEach(() => {
-      wrapper = shallow(<Carousel slides={slides} />);
+      mounted = mount(<Carousel slides={slides} />);
+    });
+
+    it('passes `slides` down to the core component', () => {
+      expect(mounted.find(CoreCarousel).prop('slides')).toBe(slides);
     });
 
     it('sets slideIndex={0} on the core component', () => {
-      expect(wrapper.find(CoreCarousel).prop('slideIndex')).toBe(0);
+      expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(0);
     });
 
     it('allows `slideIndex` to be controlled', () => {
@@ -43,8 +47,15 @@ describe('Carousel', () => {
       expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(0);
     });
 
-    it('passes `slides` down to the core component', () => {
-      expect(wrapper.find(CoreCarousel).prop('slides')).toBe(slides);
+    it('advances the slide `afterAdvanceDelay` elapes', () => {
+      jest.useFakeTimers();
+      const autoAdvanceDelay = 10e3;
+      mounted = mount(
+        <Carousel slide={slides} autoAdvanceDelay={autoAdvanceDelay} />
+      );
+      jest.advanceTimersByTime(autoAdvanceDelay);
+      mounted.update();
+      expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(1);
     });
   });
 
